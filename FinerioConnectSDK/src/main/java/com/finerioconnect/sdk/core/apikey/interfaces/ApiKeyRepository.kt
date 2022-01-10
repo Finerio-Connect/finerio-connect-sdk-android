082d0eb191f156d210b17070820bd0fc87af31e0
Erick-dominguez <erick@finerioconnect.com>
2022-01-09 21:18:18 -0600

    added method in interface to set listener

--- FinerioConnectSDK/src/main/java/com/finerioconnect/sdk/core/apikey/interfaces/ApiKeyRepository.kt
+++ FinerioConnectSDK/src/main/java/com/finerioconnect/sdk/core/apikey/interfaces/ApiKeyRepository.kt
@@ -2,4 +2,5 @@ package com.finerioconnect.sdk.core.apikey.interfaces
 
 interface ApiKeyRepository {
     fun validateApiKey(apiKey: String)
+    fun setListener(listener: OnApiKeyRepositoryListener?)
 }
